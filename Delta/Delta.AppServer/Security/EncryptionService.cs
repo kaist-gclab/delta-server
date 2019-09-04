1ae3ad04609357bd8d379a41ac814e12e74b7e7e
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-09-04 19:24:56 +0900

    아직 구현되지 않은 EncryptionService를 추가합니다

---
+++ Delta/Delta.AppServer/Security/EncryptionService.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Delta.AppServer.Security
+{
+    public class EncryptionService
+    {
+        public byte[] Encrypt(byte[] plainData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Decrypt(byte[] cipherData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Delta/Delta.AppServer/Startup.cs
+++ Delta/Delta.AppServer/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Delta.AppServer.Security;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace Delta.AppServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddScoped<EncryptionService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
