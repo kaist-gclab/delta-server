16d2f50d9856ada566c360344388b023aa9a2794
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-09-04 20:42:08 +0900

    MemoryObjectStorageService 및 MemoryObjectStorageServiceTest를 구현합니다

---
+++ Delta/Delta.AppServer.Test/Assets/MemoryObjectStorageServiceTest.cs
@@ -0,0 +1,39 @@
+using System;
+using Delta.AppServer.Assets;
+using Xunit;
+
+namespace Delta.AppServer.Test.Assets
+{
+    public class MemoryObjectStorageServiceTest
+    {
+        [Fact]
+        public async void Write()
+        {
+            var service = new MemoryObjectStorageService();
+            await Assert.ThrowsAsync<ArgumentNullException>(() => service.Write(null, null));
+            await Assert.ThrowsAsync<ArgumentNullException>(() => service.Write("", null));
+            await Assert.ThrowsAsync<ArgumentNullException>(() => service.Write(null, new byte[] { }));
+            await Assert.ThrowsAsync<ArgumentOutOfRangeException>(() => service.Write("", new byte[] { }));
+            await Assert.ThrowsAsync<ArgumentNullException>(() => service.Write(null, new byte[] {1}));
+            await Assert.ThrowsAsync<ArgumentOutOfRangeException>(() => service.Write("", new byte[] {1}));
+            await Assert.ThrowsAsync<ArgumentNullException>(() => service.Write("a", null));
+            await service.Write("a", new byte[] { });
+            await service.Write("a", new byte[] {1});
+        }
+
+        [Fact]
+        public async void Read()
+        {
+            var service = new MemoryObjectStorageService();
+            await service.Write("a", new byte[] { });
+            Assert.Empty(await service.Read("a"));
+            await service.Write("a", new byte[] {1});
+            Assert.Single(await service.Read("a"));
+            var bytes = new byte[] {3};
+            await service.Write("a", bytes);
+            bytes[0] = 5;
+            Assert.Equal(5, bytes[0]);
+            Assert.Equal(3, (await service.Read("a"))[0]);
+        }
+    }
+}
---
+++ Delta/Delta.AppServer/Assets/MemoryObjectStorageService.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Delta.AppServer.Assets
+{
+    public class MemoryObjectStorageService : IObjectStorageService
+    {
+        public Dictionary<string, byte[]> Storage { get; set; } = new Dictionary<string, byte[]>();
+
+        public Task Write(string key, byte[] content)
+        {
+            if (key == null || content == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (key == "")
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            Storage[key] = (byte[]) content.Clone();
+            return Task.CompletedTask;
+        }
+
+        public Task<byte[]> Read(string key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (key == "")
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return Task.FromResult((byte[]) Storage[key].Clone());
+        }
+    }
+}
