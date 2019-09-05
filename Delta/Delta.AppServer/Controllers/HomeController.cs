d9e835876389576d9819e2feace10d2e1055d922
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-09-05 16:53:33 +0900

    ApiController 특성을 추가합니다

--- Delta/Delta.AppServer/Controllers/HomeController.cs
+++ Delta/Delta.AppServer/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Delta.AppServer.Controllers
 {
     [Route("/")]
+    [ApiController]
     public class HomeController : ControllerBase
     {
         public string Home()
