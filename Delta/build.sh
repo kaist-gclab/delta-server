2efab759520acaad31d2b9dd3313d688fd450822
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-09-06 00:28:31 +0900

    빌드 및 테스트를 수행합니다

--- Delta/build.sh
+++ Delta/build.sh
@@ -1,5 +1,1 @@
-cd Delta.AppServer && \
-rm -rf ./bin/Release/ && \
-dotnet publish --self-contained --configuration Release --runtime linux-musl-x64 && \
-cd bin/Release && \
-docker build -t delta-app-server -f ../../../Dockerfile .
+docker build -t delta-app-server .
