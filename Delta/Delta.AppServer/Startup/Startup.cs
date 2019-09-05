6e3135f4c091e5efc364dc3a31b02e380ee3f5c4
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-09-05 15:14:52 +0900

    리버스 프록시 사용 예정이므로 HTTPS 및 HSTS 관련 내용을 제거합니다

--- Delta/Delta.AppServer/Startup/Startup.cs
+++ Delta/Delta.AppServer/Startup/Startup.cs
@@ -40,13 +40,7 @@ namespace Delta.AppServer.Startup
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                // app.UseHsts();
-            }
 
-            // app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
