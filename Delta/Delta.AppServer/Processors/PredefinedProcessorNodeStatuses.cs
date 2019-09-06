c511f378f8837fe5bae0652f403eaee95ac2109b
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-09-06 17:56:09 +0900

    사전 정의된 처리기 노드 상태를 추가합니다

---
+++ Delta/Delta.AppServer/Processors/PredefinedProcessorNodeStatuses.cs
@@ -0,0 +1,9 @@
+namespace Delta.AppServer.Processors
+{
+    public static class PredefinedProcessorNodeStatuses
+    {
+        public const string Available = "Available";
+        public const string Busy = "Busy";
+        public const string Down = "Down";
+    }
+}
