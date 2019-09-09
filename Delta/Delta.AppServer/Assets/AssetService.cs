8997dbe29d1994958078fb923c45b3c2abb0b99c
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-09-10 04:15:53 +0900

    AssetService에 IClock을 주입합니다

--- Delta/Delta.AppServer/Assets/AssetService.cs
+++ Delta/Delta.AppServer/Assets/AssetService.cs
@@ -1,12 +1,16 @@
+using NodaTime;
+
 namespace Delta.AppServer.Assets
 {
     public class AssetService
     {
         private readonly DeltaContext _context;
+        private readonly IClock _clock;
 
-        public AssetService(DeltaContext context)
+        public AssetService(DeltaContext context, IClock clock)
         {
             _context = context;
+            _clock = clock;
         }
     }
 }
