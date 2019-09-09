b0be04eec30ed677241ebcaa6ac1c41eefc26de4
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-09-10 04:15:01 +0900

    JobService에 IClock을 주입합니다

--- Delta/Delta.AppServer/Jobs/JobService.cs
+++ Delta/Delta.AppServer/Jobs/JobService.cs
@@ -1,12 +1,16 @@
+using NodaTime;
+
 namespace Delta.AppServer.Jobs
 {
     public class JobService
     {
         private readonly DeltaContext _context;
+        private readonly IClock _clock;
 
-        public JobService(DeltaContext context)
+        public JobService(DeltaContext context, IClock clock)
         {
             _context = context;
+            _clock = clock;
         }
     }
 }
