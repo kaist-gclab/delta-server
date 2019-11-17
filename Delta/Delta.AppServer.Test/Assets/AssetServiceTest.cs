26118cf1272136df3f6282b7910968ac454f5ad4
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-11-18 05:49:17 +0900

    주요 서비스 테스트 작성을 준비합니다

---
+++ Delta/Delta.AppServer.Test/Assets/AssetServiceTest.cs
@@ -0,0 +1,20 @@
+using System;
+using Delta.AppServer.Test.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Delta.AppServer.Test.Assets
+{
+    public class AssetServiceTest : ServiceTest
+    {
+        public AssetServiceTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void AddAsset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Delta/Delta.AppServer.Test/Jobs/JobServiceTest.cs
@@ -0,0 +1,32 @@
+using System;
+using Delta.AppServer.Test.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Delta.AppServer.Test.Jobs
+{
+    public class JobServiceTest : ServiceTest
+    {
+        public JobServiceTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void AddJob()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ScheduleNextJob()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void AddJobResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
