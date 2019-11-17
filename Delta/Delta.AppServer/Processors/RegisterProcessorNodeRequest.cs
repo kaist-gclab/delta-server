eecbf2c602c5254d9842e3111634f24d9c80d587
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-11-18 07:06:43 +0900

    처리기 노드의 등록 요청에는 RegisterProcessorNodeRequest가 포함됩니다

---
+++ Delta/Delta.AppServer/Processors/RegisterProcessorNodeRequest.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace Delta.AppServer.Processors
+{
+    public class RegisterProcessorNodeRequest
+    {
+        public string ProcessorTypeKey { get; set; }
+        public string ProcessorVersionKey { get; set; }
+        public string ProcessorVersionDescription { get; set; }
+        public string ProcessorNodeKey { get; set; }
+        public string ProcessorNodeName { get; set; }
+
+        public List<InputCapability> InputCapabilities { get; set; }
+        
+        public class InputCapability
+        {
+            public string AssetFormatKey { get; set; }
+            public string AssetTypeKey { get; set; }
+        }
+    }
+}
