08673eea81509f49ab33ecfef6a8821ae92cabc1
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-11-20 07:53:49 +0900

    변경된 데이터 모델을 반영하는 마이그레이션을 수행합니다

---
+++ Delta/Delta.AppServer/Migrations/20191119225211_JobProcessorVersion.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Delta.AppServer.Migrations
+{
+    public partial class JobProcessorVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_job_processor_type_processor_type_id",
+                table: "job");
+
+            migrationBuilder.DropIndex(
+                name: "IX_job_processor_type_id",
+                table: "job");
+
+            migrationBuilder.DropColumn(
+                name: "processor_type_id",
+                table: "job");
+
+            migrationBuilder.AddColumn<long>(
+                name: "processor_version_id",
+                table: "job",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_job_processor_version_id",
+                table: "job",
+                column: "processor_version_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_job_processor_version_processor_version_id",
+                table: "job",
+                column: "processor_version_id",
+                principalTable: "processor_version",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_job_processor_version_processor_version_id",
+                table: "job");
+
+            migrationBuilder.DropIndex(
+                name: "IX_job_processor_version_id",
+                table: "job");
+
+            migrationBuilder.DropColumn(
+                name: "processor_version_id",
+                table: "job");
+
+            migrationBuilder.AddColumn<long>(
+                name: "processor_type_id",
+                table: "job",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_job_processor_type_id",
+                table: "job",
+                column: "processor_type_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_job_processor_type_processor_type_id",
+                table: "job",
+                column: "processor_type_id",
+                principalTable: "processor_type",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
