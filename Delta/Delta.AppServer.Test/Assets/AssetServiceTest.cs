1322b675af87f1483787dc96e88372487a2ef641
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-11-20 03:41:29 +0900

    테스트 AddAsset을 추가합니다

--- Delta/Delta.AppServer.Test/Assets/AssetServiceTest.cs
+++ Delta/Delta.AppServer.Test/Assets/AssetServiceTest.cs
@@ -1,5 +1,10 @@
 using System;
+using Delta.AppServer.Assets;
+using Delta.AppServer.ObjectStorage;
+using Delta.AppServer.Security;
 using Delta.AppServer.Test.Infrastructure;
+using NodaTime;
+using NodaTime.Testing;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -12,9 +17,39 @@ namespace Delta.AppServer.Test.Assets
         }
 
         [Fact]
-        public void AddAsset()
+        public async void AddAsset()
         {
-            throw new NotImplementedException();
+            var context = CreateDbContext();
+            var clock = new FakeClock(Instant.FromUtc(2010, 8, 15, 23, 30));
+            var encryptionService = new EncryptionService(context, Output.ToLogger<EncryptionService>());
+            var objectStorage = new MemoryObjectStorageService();
+            var service = new AssetService(context, clock, objectStorage, encryptionService);
+
+            var assetFormat = new AssetFormat
+            {
+                Key = "format-a",
+                Name = "Format A",
+                Description = "Format a."
+            };
+            var assetType = new AssetType
+            {
+                Key = "type-a",
+                Name = "Type A"
+            };
+            var content = new byte[] {0x12, 0x34, 0x56, 0x78, 0xAB};
+            var encryptionKey = new EncryptionKey
+            {
+                Name = "Key a",
+                Value = "a",
+                Enabled = false
+            };
+            await Assert.ThrowsAsync<Exception>(async () =>
+                await service.AddAsset(assetFormat, assetType, content, encryptionKey, null));
+            encryptionKey.Enabled = true;
+            await service.AddAsset(assetFormat, assetType, content, encryptionKey, null);
+            Assert.Single(objectStorage.Storage);
+            await service.AddAsset(assetFormat, assetType, content, null, null);
+            Assert.Equal(2, objectStorage.Storage.Count);
         }
     }
 }
--- Delta/Delta.AppServer/Assets/AssetService.cs
+++ Delta/Delta.AppServer/Assets/AssetService.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Delta.AppServer.Jobs;
+using Delta.AppServer.ObjectStorage;
+using Delta.AppServer.Security;
 using NodaTime;
 
 namespace Delta.AppServer.Assets
@@ -6,15 +12,64 @@ namespace Delta.AppServer.Assets
     {
         private readonly DeltaContext _context;
         private readonly IClock _clock;
+        private readonly IObjectStorageService _objectStorageService;
+        private readonly EncryptionService _encryptionService;
 
-        public AssetService(DeltaContext context, IClock clock)
+        public AssetService(DeltaContext context, IClock clock,
+            IObjectStorageService objectStorageService,
+            EncryptionService encryptionService)
         {
             _context = context;
             _clock = clock;
+            _objectStorageService = objectStorageService;
+            _encryptionService = encryptionService;
         }
-        public Asset GetAsset(long id)
+
+        public async Task<Asset> AddAsset(
+            AssetFormat assetFormat,
+            AssetType assetType,
+            byte[] content,
+            EncryptionKey encryptionKey,
+            JobExecution parentJobExecution)
         {
-            return _context.Assets.Find(id);
+            if (encryptionKey != null)
+            {
+                content = _encryptionService.Encrypt(encryptionKey, content);
+            }
+
+            var storeKey = Guid.NewGuid().ToString();
+            await _objectStorageService.Write(storeKey, content);
+            var asset = new Asset
+            {
+                AssetFormat = assetFormat,
+                AssetType = assetType,
+                StoreKey = storeKey,
+                EncryptionKey = encryptionKey,
+                ParentJobExecution = parentJobExecution,
+                CreatedAt = _clock.GetCurrentInstant()
+            };
+            asset = (await _context.AddAsync(asset)).Entity;
+            await _context.SaveChangesAsync();
+            return asset;
+        }
+
+        public Asset GetAsset(long id) => _context.Assets.Find(id);
+        public AssetFormat GetAssetFormat(long id) => _context.AssetFormats.Find(id);
+        public AssetType GetAssetType(long id) => _context.AssetTypes.Find(id);
+
+
+        public AssetFormat GetAssetFormat(string key)
+        {
+            return (from f in _context.AssetFormats
+                    where f.Key == key
+                    select f).First();
+        }
+
+        public AssetType GetAssetType(string key)
+        {
+            return (from t in _context.AssetTypes
+                    where t.Key == key
+                    select t).First();
         }
     }
 }
