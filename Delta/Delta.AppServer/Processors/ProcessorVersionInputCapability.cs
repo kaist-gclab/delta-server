ec3515a1a3464bb6df21f980c20268524f1249c8
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-11-20 06:54:07 +0900

    에셋 형식과 에셋 유형이 모두 null인 처리기 버전 입력 능력을 허용합니다

--- Delta/Delta.AppServer/Processors/ProcessorVersionInputCapability.cs
+++ Delta/Delta.AppServer/Processors/ProcessorVersionInputCapability.cs
@@ -7,11 +7,11 @@ namespace Delta.AppServer.Processors
     {
         public long Id { get; set; }
         public long ProcessorVersionId { get; set; }
-        public long AssetFormatId { get; set; }
+        public long? AssetFormatId { get; set; }
         public long? AssetTypeId { get; set; }
 
         [Required] public virtual ProcessorVersion ProcessorVersion { get; set; }
-        [Required] public virtual AssetFormat AssertFormat { get; set; }
+        public virtual AssetFormat AssertFormat { get; set; }
         public virtual AssetType AssertType { get; set; }
     }
 }
