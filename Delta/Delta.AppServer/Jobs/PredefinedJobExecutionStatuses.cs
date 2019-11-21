3040ebf44deb87f0605593c8ed9d0bf709cc6463
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-11-21 15:41:26 +0900

    사전 정의된 작업 실행 상태를 추가합니다

---
+++ Delta/Delta.AppServer/Jobs/PredefinedJobExecutionStatuses.cs
@@ -0,0 +1,9 @@
+namespace Delta.AppServer.Jobs
+{
+    public static class PredefinedJobExecutionStatuses
+    {
+        public const string Assigned = "Assigned";
+        public const string Complete = "Complete";
+        public const string Failed = "Failed";
+    }
+}
