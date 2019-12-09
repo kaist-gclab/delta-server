e3b8c126e7fd9e267170d36c36cb496eec3e427c
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-12-09 19:58:59 +0900

    작업 할당 요청 API를 추가합니다

--- Delta/Delta.AppServer/Jobs/JobsController.cs
+++ Delta/Delta.AppServer/Jobs/JobsController.cs
@@ -1,10 +1,31 @@
+using Delta.AppServer.Processors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Delta.AppServer.Jobs
 {
     [ApiController]
+    [Route(Delta.ApiRoot + "jobs")]
     public class JobsController : ControllerBase
     {
-        
+        private readonly JobService _jobService;
+        private readonly ProcessorService _processorService;
+
+        public JobsController(JobService jobService, ProcessorService processorService)
+        {
+            _jobService = jobService;
+            _processorService = processorService;
+        }
+
+        [HttpGet]
+        public IActionResult Schedule(long processorNodeId)
+        {
+            var processorNode = _processorService.GetNode(processorNodeId);
+            if (processorNode == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_jobService.ScheduleNextJob(processorNode));
+        }
     }
 }
