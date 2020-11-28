ca1c091e2c9b9f316d232edf5c02bb79129f5bf2
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 01:51:10 +0900

    JobTypesController 준비

---
+++ Delta/Delta.AppServer/Jobs/JobTypesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Delta.AppServer.Jobs
+{
+    [ApiController]
+    [Route("job-types")]
+    public class JobTypesController : ControllerBase
+    {
+    }
+}
