e013ed9734a8d2a41219efc19ee00353f1a85533
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 03:46:14 +0900

    사용하지 않는 코드 제거

--- Delta/Delta.AppServer/Assets/CreateAssetFormatRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace Delta.AppServer.Assets
-{
-    public class CreateAssetFormatRequest
-    {
-        public string Key { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
--- Delta/Delta.AppServer/Assets/UpdateAssetTagRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace Delta.AppServer.Assets
-{
-    public class UpdateAssetTagRequest
-    {
-        public string Key { get; set; }
-        public string Value { get; set; }
-    }
-}
