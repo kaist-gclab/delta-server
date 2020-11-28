f2affa57f7e26f073877e3c533b15989f4bb7399
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 18:54:07 +0900

    변경된 Job 데이터 모델 적용

--- Delta/Delta.AppServer/Jobs/Job.cs
+++ Delta/Delta.AppServer/Jobs/Job.cs
@@ -9,13 +9,15 @@ namespace Delta.AppServer.Jobs
     public class Job
     {
         public long Id { get; set; }
-        public long ProcessorVersionId { get; set; }
+        public long JobTypeId { get; set; }
         public long? InputAssetId { get; set; }
         [Required] public string JobArguments { get; set; }
         public Instant CreatedAt { get; set; }
+        public long? AssignedProcessorNodeId { get; set; }
 
-        [Required] public virtual ProcessorVersion ProcessorVersion { get; set; }
+        [Required] public virtual JobType JobType { get; set; }
         public virtual Asset InputAsset { get; set; }
         public virtual ICollection<JobExecution> JobExecutions { get; set; } = new HashSet<JobExecution>();
+        public virtual ProcessorNode AssignedProcessorNode { get; set; }
     }
 }
