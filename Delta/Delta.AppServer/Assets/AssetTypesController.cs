ad9a3ec15d9ebae2b292eea138fccf570ce66934
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 01:47:52 +0900

    AssetTypesController 준비

---
+++ Delta/Delta.AppServer/Assets/AssetTypesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Delta.AppServer.Assets
+{
+    [ApiController]
+    [Route("asset-types")]
+    public class AssetTypesController : ControllerBase
+    {
+    }
+}
