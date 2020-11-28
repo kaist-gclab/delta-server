afec7938e61b57cb6389ad7a1ee43e02bf7e89d4
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 18:53:08 +0900

    변경된 Asset 데이터 모델 적용

--- Delta/Delta.AppServer/Assets/Asset.cs
+++ Delta/Delta.AppServer/Assets/Asset.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Delta.AppServer.Jobs;
 using Delta.AppServer.Encryption;
 using NodaTime;
@@ -8,17 +9,17 @@ namespace Delta.AppServer.Assets
     public class Asset
     {
         public long Id { get; set; }
-        public long? AssetFormatId { get; set; }
+        public long AssetTypeId { get; set; }
         public long? EncryptionKeyId { get; set; }
-        public long? AssetTypeId { get; set; }
-        public string StoreKey { get; set; }
+        [Required] public string MediaType { get; set; }
+        [Required] public string StoreKey { get; set; }
         public long? ParentJobExecutionId { get; set; }
         public Instant CreatedAt { get; set; }
 
-        public virtual AssetFormat AssetFormat { get; set; }
-        public virtual AssetType AssetType { get; set; }
-        public virtual JobExecution ParentJobExecution { get; set; }
+        [Required] public virtual AssetType AssetType { get; set; }
         public virtual EncryptionKey EncryptionKey { get; set; }
+
+        public virtual JobExecution ParentJobExecution { get; set; }
         public virtual ICollection<Job> InputJobs { get; set; }
         public virtual ICollection<AssetTag> AssetTags { get; set; } = new HashSet<AssetTag>();
     }
