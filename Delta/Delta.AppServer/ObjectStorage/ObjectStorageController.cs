b001042faffc866e0abab5c274843089386b4318
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 03:42:36 +0900

    사전 서명된 업로드 URL 서비스 시작

---
+++ Delta/Delta.AppServer/ObjectStorage/ObjectStorageController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Delta.AppServer.ObjectStorage
+{
+    [ApiController]
+    [Route(Delta.ApiRoot + "object-storage")]
+    public class ObjectStorageController : ControllerBase
+    {
+        private readonly IObjectStorageService _objectStorageService;
+
+        public ObjectStorageController(IObjectStorageService objectStorageService)
+        {
+            _objectStorageService = objectStorageService;
+        }
+
+        [HttpGet("upload-url")]
+        public async Task<UploadTicket> GetUploadUrl()
+        {
+            var storeKey = Guid.NewGuid().ToString();
+            var url = await _objectStorageService.GetPresignedUploadUrl(storeKey);
+            return new UploadTicket
+            {
+                Url = url,
+                StoreKey = storeKey
+            };
+        }
+    }
+}
