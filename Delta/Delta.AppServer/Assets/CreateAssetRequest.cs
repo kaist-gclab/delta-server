27b1037b0700bbb4bdd198cb9c7291a45d871004
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 03:45:06 +0900

    에셋 추가 요청 타입 추가

--- Delta/Delta.AppServer/Assets/CreateAssetRequest.cs
+++ Delta/Delta.AppServer/Assets/CreateAssetRequest.cs
@@ -1,10 +1,14 @@
+using System.Collections.Generic;
+
 namespace Delta.AppServer.Assets
 {
     public class CreateAssetRequest
     {
-        public string AssetFormatKey { get; set; }
-        public string AssetTypeKey { get; set; }
-        public byte[] Content { get; set; }
-        public string EncryptionKeyName { get; set; }
+        public long AssetTypeId { get; set; }
+        public long? EncryptionKeyId { get; set; }
+        public string MediaType { get; set; }
+        public string StoreKey { get; set; }
+        public IEnumerable<CreateAssetTagRequest> CreateAssetTagRequest { get; set; }
+        public long? ParentJobExecutionId { get; set; }
     }
 }
