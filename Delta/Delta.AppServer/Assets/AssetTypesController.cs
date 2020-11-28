4363576bf35438230556e5f5b07aa7cd6e634f9a
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 03:07:06 +0900

    에셋 유형 목록 구현 완료

--- Delta/Delta.AppServer/Assets/AssetTypesController.cs
+++ Delta/Delta.AppServer/Assets/AssetTypesController.cs
@@ -1,10 +1,23 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Delta.AppServer.Assets
 {
     [ApiController]
-    [Route("asset-types")]
+    [Route(Delta.ApiRoot + "asset-types")]
     public class AssetTypesController : ControllerBase
     {
+        private readonly AssetMetadataService _assetMetadataService;
+
+        public AssetTypesController(AssetMetadataService assetMetadataService)
+        {
+            _assetMetadataService = assetMetadataService;
+        }
+
+        [HttpGet]
+        public IEnumerable<AssetType> GetAssetTypes()
+        {
+            return _assetMetadataService.GetAssetTypes();
+        }
     }
 }
