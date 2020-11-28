9ee4cc62aa59d9454cadf6f1765266d99aa16e9d
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 22:35:41 +0900

    LoginRequest 분리

---
+++ Delta/Delta.AppServer/Core/Security/LoginRequest.cs
@@ -0,0 +1,8 @@
+namespace Delta.AppServer.Core.Security
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
