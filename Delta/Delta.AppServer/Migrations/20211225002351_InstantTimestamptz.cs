11cedf2afb0211302e3dfc6b0478977a964063ac
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-25 09:24:02 +0900

    InstantTimestamptz

---
+++ Delta/Delta.AppServer/Migrations/20211225002351_InstantTimestamptz.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+
+#nullable disable
+
+namespace Delta.AppServer.Migrations
+{
+    public partial class InstantTimestamptz : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Instant>(
+                name: "timestamp",
+                table: "processor_node_status",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(Instant),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<Instant>(
+                name: "timestamp",
+                table: "job_execution_status",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(Instant),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<Instant>(
+                name: "created_at",
+                table: "job",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(Instant),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<Instant>(
+                name: "created_at",
+                table: "asset",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(Instant),
+                oldType: "timestamp without time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Instant>(
+                name: "timestamp",
+                table: "processor_node_status",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(Instant),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<Instant>(
+                name: "timestamp",
+                table: "job_execution_status",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(Instant),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<Instant>(
+                name: "created_at",
+                table: "job",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(Instant),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<Instant>(
+                name: "created_at",
+                table: "asset",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(Instant),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
