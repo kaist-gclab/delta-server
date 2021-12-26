6cbe58f3e8e82b331243c7e1c06ee4c4f221429a
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 10:24:32 +0900

    AuthInfo를 record로 변경

--- Delta/Delta.AppServer/Core/Security/AuthInfo.cs
+++ Delta/Delta.AppServer/Core/Security/AuthInfo.cs
@@ -2,9 +2,5 @@ using Delta.AppServer.Users;
 
 namespace Delta.AppServer.Core.Security
 {
-    public class AuthInfo
-    {
-        public User User { get; set; }
-        public string Role { get; set; }
-    }
+    public record AuthInfo(User User, string Role);
 }
