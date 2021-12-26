b3aeeb3d666ef42fe5cf23091c0a70ed898d7228
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 10:41:12 +0900

    UserService 코드 품질 향상

--- Delta/Delta.AppServer/Users/UserService.cs
+++ Delta/Delta.AppServer/Users/UserService.cs
@@ -2,96 +2,95 @@ using System;
 using System.Linq;
 using Delta.AppServer.Core.Security;
 
-namespace Delta.AppServer.Users
+namespace Delta.AppServer.Users;
+
+public class UserService
 {
-    public class UserService
+    private readonly DeltaContext _context;
+    private readonly AuthConfig _authConfig;
+
+    public UserService(DeltaContext context, AuthConfig authConfig)
     {
-        private readonly DeltaContext _context;
-        private readonly AuthConfig _authConfig;
+        _context = context;
+        _authConfig = authConfig;
+    }
 
-        public UserService(DeltaContext context, AuthConfig authConfig)
+    public User? Login(string username, string password)
+    {
+        var user = GetUserByUsername(username);
+        if (user == null)
         {
-            _context = context;
-            _authConfig = authConfig;
+            return null;
         }
 
-        public User? Login(string username, string password)
+        if (user.Username == _authConfig.AdminUsername)
         {
-            var user = GetUserByUsername(username);
-            if (user == null)
-            {
-                return null;
-            }
-
-            if (user.Username == _authConfig.AdminUsername)
-            {
-                return password == _authConfig.AdminPassword ? user : null;
-            }
-
-            return user.ValidatePassword(password) ? user : null;
+            return password == _authConfig.AdminPassword ? user : null;
         }
 
-        public User AddUser(string username, string password, string name)
+        return user.ValidatePassword(password) ? user : null;
+    }
+
+    public User AddUser(string username, string password, string name)
+    {
+        if (username == _authConfig.AdminUsername)
         {
-            if (username == _authConfig.AdminUsername)
-            {
-                return GetUserByUsername(_authConfig.AdminUsername);
-            }
+            return GetUserByUsername(_authConfig.AdminUsername);
+        }
 
-            using var trx = _context.Database.BeginTransaction();
+        using var trx = _context.Database.BeginTransaction();
 
-            var duplicates = from u in _context.Users
-                where u.Username == username
-                select u;
-            if (duplicates.Any())
-            {
-                throw new Exception();
-            }
+        var duplicates = from u in _context.Users
+            where u.Username == username
+            select u;
+        if (duplicates.Any())
+        {
+            throw new Exception();
+        }
 
-            var user = new User
+        var user = new User
+        {
+            Username = username,
+            Name = name
+        };
+        user.ChangePassword(password);
+
+        user = _context.Add(user).Entity;
+        _context.SaveChanges();
+        trx.Commit();
+        return user;
+    }
+
+    public void ChangePassword(string username, string newPassword)
+    {
+        if (username == _authConfig.AdminUsername)
+        {
+            return;
+        }
+
+        var user = GetUserByUsername(username);
+        user?.ChangePassword(newPassword);
+        _context.SaveChanges();
+    }
+
+    public string GetRole(User user)
+    {
+        return user.Username == _authConfig.AdminUsername ? "Admin" : "User";
+    }
+
+    private User? GetUserByUsername(string username)
+    {
+        if (username == _authConfig.AdminUsername)
+        {
+            return new User
             {
-                Username = username,
-                Name = name
+                Username = _authConfig.AdminUsername,
+                Name = _authConfig.AdminUsername
             };
-            user.ChangePassword(password);
-
-            user = _context.Add(user).Entity;
-            _context.SaveChanges();
-            trx.Commit();
-            return user;
         }
 
-        public void ChangePassword(string username, string newPassword)
-        {
-            if (username == _authConfig.AdminUsername)
-            {
-                return;
-            }
-
-            var user = GetUserByUsername(username);
-            user?.ChangePassword(newPassword);
-            _context.SaveChanges();
-        }
-
-        public string GetRole(User user)
-        {
-            return user.Username == _authConfig.AdminUsername ? "Admin" : "User";
-        }
-
-        private User? GetUserByUsername(string username)
-        {
-            if (username == _authConfig.AdminUsername)
-            {
-                return new User
-                {
-                    Username = _authConfig.AdminUsername,
-                    Name = _authConfig.AdminUsername
-                };
-            }
-
-            return (from u in _context.Users
-                where u.Username == username
-                select u).FirstOrDefault();
-        }
+        return (from u in _context.Users
+            where u.Username == username
+            select u).FirstOrDefault();
     }
 }
