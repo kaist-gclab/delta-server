fd161ed8b4936a9d283e07f3f1d12d8442d050a7
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 10:08:39 +0900

    코드 품질 개선

--- Delta/Delta/Delta.cs
+++ Delta/Delta/Delta.cs
@@ -1,9 +1,8 @@
-﻿namespace Delta
+﻿namespace Delta;
+
+public class Delta
 {
-    public class Delta
-    {
-        public static string ServiceName => "Delta";
-        public static string ApiVersion => "3";
-        public const string ApiRoot = "/api/1/";
-    }
+    public static string ServiceName => "Delta";
+    public static string ApiVersion => "3";
+    public const string ApiRoot = "/api/1/";
 }
