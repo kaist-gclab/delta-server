77712c77076997186099fdf118c6d585698c285c
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 10:45:27 +0900

    Startup 코드 품질 향상

--- Delta/Delta.AppServer/Startup/Startup.cs
+++ Delta/Delta.AppServer/Startup/Startup.cs
@@ -25,123 +25,122 @@ using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using NodaTime;
 
-namespace Delta.AppServer.Startup
+namespace Delta.AppServer.Startup;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
+        _configuration = configuration;
+    }
 
-        private readonly IConfiguration _configuration;
+    private readonly IConfiguration _configuration;
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddResponseCompression();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = false,
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey =
-                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Secret"])),
-                        ValidIssuer = _configuration["Jwt:Issuer"],
-                        ValidAudience = _configuration["Jwt:Issuer"],
-                        ValidAlgorithms = new[] {"HS256"}
-                    };
-                });
-
-            services.AddCors();
-
-            services.AddMvc(options =>
-                {
-                    var policy = new AuthorizationPolicyBuilder()
-                        .RequireAssertion(context =>
-                        {
-                            var json = (from c in context.User.Claims
-                                where c.Type == "authInfo"
-                                select c.Value).FirstOrDefault();
-
-                            if (json == null)
-                            {
-                                return false;
-                            }
-
-                            try
-                            {
-                                var authInfo = JsonConvert.DeserializeObject<AuthInfo>(json);
-                                return authInfo.Role == "Admin";
-                            }
-                            catch
-                            {
-                                return false;
-                            }
-                        }).Build();
-                    options.Filters.Add(new AuthorizeFilter(policy));
-                })
-                .AddNewtonsoftJson(options => { options.SerializerSettings.ConfigureJsonSerializerSettings(); })
-                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
-                .AddApplicationPart(Assembly.GetAssembly(typeof(Startup)));
-            services.AddScoped<EncryptionService>();
-            services.AddSingleton<IClock>(SystemClock.Instance);
-            services.AddLogging(builder => builder.AddConsole());
-            services.AddDbContext<DeltaContext>(options =>
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddResponseCompression();
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
             {
-                options.UseLazyLoadingProxies();
-                ConfigureDbContext(options);
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey =
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Secret"])),
+                    ValidIssuer = _configuration["Jwt:Issuer"],
+                    ValidAudience = _configuration["Jwt:Issuer"],
+                    ValidAlgorithms = new[] {"HS256"}
+                };
             });
 
-            services.AddScoped<AuthConfig>();
-            services.AddScoped<ObjectStorageConfig>();
-            services.AddScoped<JobExecutionConfig>();
+        services.AddCors();
 
-            services.AddScoped<UserService>();
-            services.AddScoped<TokenService>();
-            services.AddScoped<JobService>();
-            services.AddScoped<ProcessorService>();
-            services.AddScoped<AssetService>();
-            services.AddScoped<AssetMetadataService>();
-            services.AddScoped<IObjectStorageService, S3CompatibleObjectStorageService>();
-            services.AddScoped<CompressionService>();
-            services.AddScoped<EncryptionService>();
-            services.AddScoped<IObjectStorageKeyConverter, PrefixFourObjectStorageKeyConverter>();
-            services.AddScoped<MonitoringService>();
-
-            services.AddSingleton<ScheduleHelper>();
-            services.AddSingleton(DateTimeZoneProviders.Tzdb[_configuration["Time:DateTimeZone"]]);
-        }
-
-        protected virtual void ConfigureDbContext(DbContextOptionsBuilder options)
-        {
-            options.UseNpgsql(
-                _configuration.GetConnectionString("DeltaDatabase"),
-                o => o.UseNodaTime());
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseCors(builder => builder
-                .AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader());
-
-            if (env.IsDevelopment())
+        services.AddMvc(options =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAssertion(context =>
+                    {
+                        var json = (from c in context.User.Claims
+                            where c.Type == "authInfo"
+                            select c.Value).FirstOrDefault();
 
-            app.UseRouting();
+                        if (json == null)
+                        {
+                            return false;
+                        }
 
-            app.UseAuthentication();
+                        try
+                        {
+                            var authInfo = JsonConvert.DeserializeObject<AuthInfo>(json);
+                            return authInfo.Role == "Admin";
+                        }
+                        catch
+                        {
+                            return false;
+                        }
+                    }).Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            })
+            .AddNewtonsoftJson(options => { options.SerializerSettings.ConfigureJsonSerializerSettings(); })
+            .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+            .AddApplicationPart(Assembly.GetAssembly(typeof(Startup)));
+        services.AddScoped<EncryptionService>();
+        services.AddSingleton<IClock>(SystemClock.Instance);
+        services.AddLogging(builder => builder.AddConsole());
+        services.AddDbContext<DeltaContext>(options =>
+        {
+            options.UseLazyLoadingProxies();
+            ConfigureDbContext(options);
+        });
 
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        services.AddScoped<AuthConfig>();
+        services.AddScoped<ObjectStorageConfig>();
+        services.AddScoped<JobExecutionConfig>();
+
+        services.AddScoped<UserService>();
+        services.AddScoped<TokenService>();
+        services.AddScoped<JobService>();
+        services.AddScoped<ProcessorService>();
+        services.AddScoped<AssetService>();
+        services.AddScoped<AssetMetadataService>();
+        services.AddScoped<IObjectStorageService, S3CompatibleObjectStorageService>();
+        services.AddScoped<CompressionService>();
+        services.AddScoped<EncryptionService>();
+        services.AddScoped<IObjectStorageKeyConverter, PrefixFourObjectStorageKeyConverter>();
+        services.AddScoped<MonitoringService>();
+
+        services.AddSingleton<ScheduleHelper>();
+        services.AddSingleton(DateTimeZoneProviders.Tzdb[_configuration["Time:DateTimeZone"]]);
+    }
+
+    protected virtual void ConfigureDbContext(DbContextOptionsBuilder options)
+    {
+        options.UseNpgsql(
+            _configuration.GetConnectionString("DeltaDatabase"),
+            o => o.UseNodaTime());
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseCors(builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
         }
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
     }
 }
