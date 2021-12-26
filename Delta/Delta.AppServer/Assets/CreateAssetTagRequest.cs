6b8129fd75ab16dc2be3968ebe0cc25a07b55eec
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 10:18:14 +0900

    CreateAssetTagRequest를 record로 변경

--- Delta/Delta.AppServer/Assets/CreateAssetTagRequest.cs
+++ Delta/Delta.AppServer/Assets/CreateAssetTagRequest.cs
@@ -1,8 +1,4 @@
 namespace Delta.AppServer.Assets
 {
-    public class CreateAssetTagRequest
-    {
-        public string Key { get; set; }
-        public string Value { get; set; }
-    }
+    public record CreateAssetTagRequest(string Key, string Value);
 }
