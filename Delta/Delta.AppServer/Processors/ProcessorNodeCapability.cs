7558b939a1ee66d8df9175db0e030c8562f69e61
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 10:49:49 +0900

    ProcessorNodeCapability 코드 품질 향상

--- Delta/Delta.AppServer/Processors/ProcessorNodeCapability.cs
+++ Delta/Delta.AppServer/Processors/ProcessorNodeCapability.cs
@@ -2,52 +2,51 @@ using System;
 using Delta.AppServer.Assets;
 using Delta.AppServer.Jobs;
 
-namespace Delta.AppServer.Processors
+namespace Delta.AppServer.Processors;
+
+public class ProcessorNodeCapability
 {
-    public class ProcessorNodeCapability
+    public ProcessorNodeCapability(
+        string mediaType, ProcessorNode processorNode, JobType jobType, AssetType assetType)
     {
-        public ProcessorNodeCapability(
-            string mediaType, ProcessorNode processorNode, JobType jobType, AssetType assetType)
-        {
-            MediaType = mediaType;
-            _processorNode = processorNode;
-            _jobType = jobType;
-            _assetType = assetType;
-        }
+        MediaType = mediaType;
+        _processorNode = processorNode;
+        _jobType = jobType;
+        _assetType = assetType;
+    }
 
-        protected ProcessorNodeCapability(string mediaType)
-        {
-            MediaType = mediaType;
-        }
+    protected ProcessorNodeCapability(string mediaType)
+    {
+        MediaType = mediaType;
+    }
 
-        public long Id { get; set; }
-        public long ProcessorNodeId { get; set; }
-        public long JobTypeId { get; set; }
-        public long? AssetTypeId { get; set; }
-        public string MediaType { get; set; }
+    public long Id { get; set; }
+    public long ProcessorNodeId { get; set; }
+    public long JobTypeId { get; set; }
+    public long? AssetTypeId { get; set; }
+    public string MediaType { get; set; }
 
-        private ProcessorNode _processorNode;
+    private ProcessorNode _processorNode;
 
-        public virtual ProcessorNode ProcessorNode
-        {
-            get => _processorNode ?? throw new InvalidOperationException(nameof(ProcessorNode));
-            set => _processorNode = value;
-        }
+    public virtual ProcessorNode ProcessorNode
+    {
+        get => _processorNode ?? throw new InvalidOperationException(nameof(ProcessorNode));
+        set => _processorNode = value;
+    }
 
-        private JobType _jobType;
+    private JobType _jobType;
 
-        public virtual JobType JobType
-        {
-            get => _jobType ?? throw new InvalidOperationException(nameof(JobType));
-            set => _jobType = value;
-        }
+    public virtual JobType JobType
+    {
+        get => _jobType ?? throw new InvalidOperationException(nameof(JobType));
+        set => _jobType = value;
+    }
 
-        private AssetType? _assetType;
+    private AssetType? _assetType;
 
-        public virtual AssetType? AssetType
-        {
-            get => _assetType;
-            set => _assetType = value;
-        }
+    public virtual AssetType? AssetType
+    {
+        get => _assetType;
+        set => _assetType = value;
     }
 }
