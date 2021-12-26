75b353731ceeefdd98f92a0b49b4aa55802b8c3a
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 11:41:22 +0900

    User 생성자 추가

--- Delta/Delta.AppServer/Users/User.cs
+++ Delta/Delta.AppServer/Users/User.cs
@@ -4,12 +4,18 @@ namespace Delta.AppServer.Users
 {
     public class User
     {
-        public long Id { get; set; }
-        [Required] public string Name { get; set; }
-        [Required] public string Username { get; set; }
+        public User(string name, string username)
+        {
+            Name = name;
+            Username = username;
+        }
 
-        public string Password { get; set; }
-        public string Salt { get; set; }
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+
+        public string? Password { get; set; }
+        public string? Salt { get; set; }
 
         public void ChangePassword(string password)
         {
