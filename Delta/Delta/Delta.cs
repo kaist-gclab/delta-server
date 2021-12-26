9fe2f3b7aaaf90b22886155d73a451ab7317de62
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 10:04:21 +0900

    ApiVersion 증가

--- Delta/Delta/Delta.cs
+++ Delta/Delta/Delta.cs
@@ -3,7 +3,7 @@
     public class Delta
     {
         public static string ServiceName { get; } = "Delta";
-        public static string ApiVersion { get; } = "2";
+        public static string ApiVersion { get; } = "3";
         public const string ApiRoot = "/api/1/";
     }
 }
