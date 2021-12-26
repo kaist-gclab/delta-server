2be3c4d2ad2a813895023ea96c748d334d0c0f7f
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 11:46:55 +0900

    null 안전

--- Delta/Delta.AppServer.Test/ObjectStorage/PrefixFourObjectStorageKeyConverterTest.cs
+++ Delta/Delta.AppServer.Test/ObjectStorage/PrefixFourObjectStorageKeyConverterTest.cs
@@ -3,26 +3,24 @@ using Delta.AppServer.Test.Infrastructure;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace Delta.AppServer.Test.ObjectStorage
-{
-    public class PrefixFourObjectStorageKeyConverterTest : ServiceTest
-    {
-        public PrefixFourObjectStorageKeyConverterTest(ITestOutputHelper output) : base(output)
-        {
-        }
+namespace Delta.AppServer.Test.ObjectStorage;
 
-        [Fact]
-        public void GetKey()
-        {
-            var service = new PrefixFourObjectStorageKeyConverter();
-            Assert.Null(service.GetKey(null));
-            Assert.Equal("$/$/$/$/", service.GetKey(""));
-            Assert.Equal("1/$/$/$/1", service.GetKey("1"));
-            Assert.Equal("1/2/$/$/12", service.GetKey("12"));
-            Assert.Equal("1/2/3/$/123", service.GetKey("123"));
-            Assert.Equal("1/2/3/4/1234", service.GetKey("1234"));
-            Assert.Equal("1/2/3/4/12345", service.GetKey("12345"));
-            Assert.Equal("1/2/3/4/123456", service.GetKey("123456"));
-        }
+public class PrefixFourObjectStorageKeyConverterTest : ServiceTest
+{
+    public PrefixFourObjectStorageKeyConverterTest(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [Fact]
+    public void GetKey()
+    {
+        var service = new PrefixFourObjectStorageKeyConverter();
+        Assert.Equal("$/$/$/$/", service.GetKey(""));
+        Assert.Equal("1/$/$/$/1", service.GetKey("1"));
+        Assert.Equal("1/2/$/$/12", service.GetKey("12"));
+        Assert.Equal("1/2/3/$/123", service.GetKey("123"));
+        Assert.Equal("1/2/3/4/1234", service.GetKey("1234"));
+        Assert.Equal("1/2/3/4/12345", service.GetKey("12345"));
+        Assert.Equal("1/2/3/4/123456", service.GetKey("123456"));
     }
 }
