f5cabf52c55040bbe953957bdc9c491436a83ed3
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 10:39:19 +0900

    Setting 생성자 추가

--- Delta/Delta.AppServer/Settings/Setting.cs
+++ Delta/Delta.AppServer/Settings/Setting.cs
@@ -4,7 +4,13 @@ namespace Delta.AppServer.Settings
 {
     public class Setting
     {
-        [Key] [Required] public string Key { get; set; }
-        [Required] public string Value { get; set; }
+        public Setting(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        [Key] public string Key { get; set; }
+        public string Value { get; set; }
     }
 }
