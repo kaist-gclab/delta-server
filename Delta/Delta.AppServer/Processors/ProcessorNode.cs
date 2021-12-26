a17a1221c0aeac34ec13dd56b28477355e7ef935
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 11:03:38 +0900

    ProcessorNode 코드 품질 향상

--- Delta/Delta.AppServer/Processors/ProcessorNode.cs
+++ Delta/Delta.AppServer/Processors/ProcessorNode.cs
@@ -5,64 +5,63 @@ using System.Linq;
 using Delta.AppServer.Jobs;
 using NodaTime;
 
-namespace Delta.AppServer.Processors
+namespace Delta.AppServer.Processors;
+
+public class ProcessorNode
 {
-    public class ProcessorNode
+    public long Id { get; set; }
+    public long ProcessorVersionId { get; set; }
+    [Required] public string Key { get; set; }
+    public string Name { get; set; }
+
+    public virtual ICollection<ProcessorNodeCapability> ProcessorNodeCapabilities { get; set; } =
+        new HashSet<ProcessorNodeCapability>();
+
+    public void UpdateCapabilities(IEnumerable<CreateProcessorNodeCapability> requests)
     {
-        public long Id { get; set; }
-        public long ProcessorVersionId { get; set; }
-        [Required] public string Key { get; set; }
-        public string Name { get; set; }
-
-        public virtual ICollection<ProcessorNodeCapability> ProcessorNodeCapabilities { get; set; } =
-            new HashSet<ProcessorNodeCapability>();
-
-        public void UpdateCapabilities(IEnumerable<CreateProcessorNodeCapability> requests)
+        var removed =
+            from cap in ProcessorNodeCapabilities
+            where (from req in requests
+                where req.MediaType == cap.MediaType &&
+                      req.AssetType == cap.AssetType &&
+                      req.JobType == cap.JobType
+                select req).Any()
+            select cap;
+        foreach (var c in removed.ToList())
         {
-            var removed =
-                from cap in ProcessorNodeCapabilities
-                where (from req in requests
-                    where req.MediaType == cap.MediaType &&
-                          req.AssetType == cap.AssetType &&
-                          req.JobType == cap.JobType
-                    select req).Any()
-                select cap;
-            foreach (var c in removed.ToList())
-            {
-                ProcessorNodeCapabilities.Remove(c);
-            }
-
-            var adding = from req in requests
-                where (from cap in ProcessorNodeCapabilities
-                    where req.MediaType == cap.MediaType &&
-                          req.AssetType == cap.AssetType &&
-                          req.JobType == cap.JobType
-                    select req).Any() == false
-                select req;
-
-            foreach (var (jobType, assetType, mediaType) in adding.ToList())
-            {
-                ProcessorNodeCapabilities.Add(new ProcessorNodeCapability(mediaType, this, jobType, assetType));
-            }
+            ProcessorNodeCapabilities.Remove(c);
         }
 
-        public void AddNodeStatus(Instant timestamp, string status)
+        var adding = from req in requests
+            where (from cap in ProcessorNodeCapabilities
+                where req.MediaType == cap.MediaType &&
+                      req.AssetType == cap.AssetType &&
+                      req.JobType == cap.JobType
+                select req).Any() == false
+            select req;
+
+        foreach (var (jobType, assetType, mediaType) in adding.ToList())
         {
-            if (status != PredefinedProcessorNodeStatuses.Available &&
-                status != PredefinedProcessorNodeStatuses.Busy &&
-                status != PredefinedProcessorNodeStatuses.Down)
-            {
-                throw new Exception();
-            }
-
-            ProcessorNodeStatuses.Add(new ProcessorNodeStatus(timestamp, status, this));
+            ProcessorNodeCapabilities.Add(new ProcessorNodeCapability(mediaType, this, jobType, assetType));
         }
-
-        public virtual ICollection<JobExecution> JobExecutions { get; set; }
-
-        public virtual ICollection<ProcessorNodeStatus> ProcessorNodeStatuses { get; set; } =
-            new HashSet<ProcessorNodeStatus>();
-
-        public virtual ICollection<Job> AssignedJobs { get; set; }
     }
+
+    public void AddNodeStatus(Instant timestamp, string status)
+    {
+        if (status != PredefinedProcessorNodeStatuses.Available &&
+            status != PredefinedProcessorNodeStatuses.Busy &&
+            status != PredefinedProcessorNodeStatuses.Down)
+        {
+            throw new Exception();
+        }
+
+        ProcessorNodeStatuses.Add(new ProcessorNodeStatus(timestamp, status, this));
+    }
+
+    public virtual ICollection<JobExecution> JobExecutions { get; set; }
+
+    public virtual ICollection<ProcessorNodeStatus> ProcessorNodeStatuses { get; set; } =
+        new HashSet<ProcessorNodeStatus>();
+
+    public virtual ICollection<Job> AssignedJobs { get; set; }
 }
