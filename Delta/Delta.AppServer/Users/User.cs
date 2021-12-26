fadbc59d824d60ffc88ec8894e0c3b94bd22c052
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 11:41:36 +0900

    User 코드 품질 개선

--- Delta/Delta.AppServer/Users/User.cs
+++ Delta/Delta.AppServer/Users/User.cs
@@ -1,37 +1,34 @@
-using System.ComponentModel.DataAnnotations;
+namespace Delta.AppServer.Users;
 
-namespace Delta.AppServer.Users
+public class User
 {
-    public class User
+    public User(string name, string username)
     {
-        public User(string name, string username)
+        Name = name;
+        Username = username;
+    }
+
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Username { get; set; }
+
+    public string? Password { get; set; }
+    public string? Salt { get; set; }
+
+    public void ChangePassword(string password)
+    {
+        Salt = PasswordHelper.GenerateSalt();
+        Password = PasswordHelper.ComputeSaltedPassword(password, Salt);
+    }
+
+    public bool ValidatePassword(string password)
+    {
+        if (Password == null || Salt == null)
         {
-            Name = name;
-            Username = username;
+            return false;
         }
 
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public string Username { get; set; }
-
-        public string? Password { get; set; }
-        public string? Salt { get; set; }
-
-        public void ChangePassword(string password)
-        {
-            Salt = PasswordHelper.GenerateSalt();
-            Password = PasswordHelper.ComputeSaltedPassword(password, Salt);
-        }
-
-        public bool ValidatePassword(string password)
-        {
-            if (Password == null || Salt == null)
-            {
-                return false;
-            }
-
-            var hashed = PasswordHelper.ComputeSaltedPassword(password, Salt);
-            return Password == hashed;
-        }
+        var hashed = PasswordHelper.ComputeSaltedPassword(password, Salt);
+        return Password == hashed;
     }
 }
