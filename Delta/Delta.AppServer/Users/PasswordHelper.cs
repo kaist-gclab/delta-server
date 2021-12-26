95c7a4e455296508be8cdab8c3ff4ea6ee356e5d
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 10:39:54 +0900

    PasswordHelper 코드 품질 향상

--- Delta/Delta.AppServer/Users/PasswordHelper.cs
+++ Delta/Delta.AppServer/Users/PasswordHelper.cs
@@ -2,30 +2,29 @@ using System;
 using System.Security.Cryptography;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
-namespace Delta.AppServer.Users
+namespace Delta.AppServer.Users;
+
+public static class PasswordHelper
 {
-    public static class PasswordHelper
+    public static string GenerateSalt()
     {
-        public static string GenerateSalt()
-        {
-            var salt = new byte[128 / 8];
-            using var rng = RandomNumberGenerator.Create();
-            rng.GetBytes(salt);
-            return Convert.ToBase64String(salt);
-        }
+        var salt = new byte[128 / 8];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(salt);
+        return Convert.ToBase64String(salt);
+    }
 
-        public static string ComputeSaltedPassword(string password, string salt)
-        {
-            var saltBytes = Convert.FromBase64String(salt);
+    public static string ComputeSaltedPassword(string password, string salt)
+    {
+        var saltBytes = Convert.FromBase64String(salt);
 
-            var hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                password,
-                saltBytes,
-                KeyDerivationPrf.HMACSHA256,
-                10000,
-                256 / 8));
+        var hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password,
+            saltBytes,
+            KeyDerivationPrf.HMACSHA256,
+            10000,
+            256 / 8));
 
-            return hashed;
-        }
+        return hashed;
     }
 }
