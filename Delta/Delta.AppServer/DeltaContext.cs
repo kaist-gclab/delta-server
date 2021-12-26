d95f379d08312421407e39aec6341d8fae8fe638
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 11:37:52 +0900

    DeltaContext 코드 품질 향상

--- Delta/Delta.AppServer/DeltaContext.cs
+++ Delta/Delta.AppServer/DeltaContext.cs
@@ -7,47 +7,46 @@ using Delta.AppServer.Settings;
 using Delta.AppServer.Users;
 using Microsoft.EntityFrameworkCore;
 
-namespace Delta.AppServer
+namespace Delta.AppServer;
+
+public class DeltaContext : DbContext
 {
-    public class DeltaContext : DbContext
+    public DeltaContext(DbContextOptions<DeltaContext> options)
+        : base(options)
     {
-        public DeltaContext(DbContextOptions<DeltaContext> options)
-            : base(options)
-        {
-        }
+    }
 
-        public DbSet<Asset> Assets { get; set; }
-        public DbSet<AssetTag> AssetTags { get; set; }
-        public DbSet<AssetType> AssetTypes { get; set; }
-        public DbSet<Job> Jobs { get; set; }
-        public DbSet<JobType> JobTypes { get; set; }
-        public DbSet<JobExecution> JobExecutions { get; set; }
-        public DbSet<JobExecutionStatus> JobExecutionStatuses { get; set; }
-        public DbSet<ProcessorNode> ProcessorNodes { get; set; }
-        public DbSet<ProcessorNodeStatus> ProcessorNodeStatuses { get; set; }
-        public DbSet<ProcessorNodeCapability> ProcessorNodeCapabilities { get; set; }
-        public DbSet<EncryptionKey> EncryptionKeys { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Setting> Settings { get; set; }
+    public DbSet<Asset> Assets { get; set; }
+    public DbSet<AssetTag> AssetTags { get; set; }
+    public DbSet<AssetType> AssetTypes { get; set; }
+    public DbSet<Job> Jobs { get; set; }
+    public DbSet<JobType> JobTypes { get; set; }
+    public DbSet<JobExecution> JobExecutions { get; set; }
+    public DbSet<JobExecutionStatus> JobExecutionStatuses { get; set; }
+    public DbSet<ProcessorNode> ProcessorNodes { get; set; }
+    public DbSet<ProcessorNodeStatus> ProcessorNodeStatuses { get; set; }
+    public DbSet<ProcessorNodeCapability> ProcessorNodeCapabilities { get; set; }
+    public DbSet<EncryptionKey> EncryptionKeys { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Setting> Settings { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
         {
-            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            entityType.SetTableName(entityType.DisplayName().ToUnderscoreCase());
+            foreach (var property in entityType.GetProperties())
             {
-                entityType.SetTableName(entityType.DisplayName().ToUnderscoreCase());
-                foreach (var property in entityType.GetProperties())
-                {
-                    property.SetColumnName(property.Name.ToUnderscoreCase());
-                }
+                property.SetColumnName(property.Name.ToUnderscoreCase());
             }
         }
     }
+}
 
-    internal static class IdentifierNameHelper
+internal static class IdentifierNameHelper
+{
+    public static string ToUnderscoreCase(this string str)
     {
-        public static string ToUnderscoreCase(this string str)
-        {
-            return string.Concat(str.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x : x.ToString())).ToLower();
-        }
+        return string.Concat(str.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x : x.ToString())).ToLower();
     }
 }
