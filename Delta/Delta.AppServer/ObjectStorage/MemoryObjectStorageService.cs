f49208cd5afa10d5f4e78f3794310a1ab7868fe8
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 11:35:08 +0900

    MemoryObjectStorageService 코드 품질 향상

--- Delta/Delta.AppServer/ObjectStorage/MemoryObjectStorageService.cs
+++ Delta/Delta.AppServer/ObjectStorage/MemoryObjectStorageService.cs
@@ -2,56 +2,55 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Delta.AppServer.ObjectStorage
+namespace Delta.AppServer.ObjectStorage;
+
+public class MemoryObjectStorageService : IObjectStorageService
 {
-    public class MemoryObjectStorageService : IObjectStorageService
+    public Dictionary<string, byte[]> Storage { get; } = new();
+
+    public Task Write(string key, byte[] content)
     {
-        public Dictionary<string, byte[]> Storage { get; set; } = new Dictionary<string, byte[]>();
-
-        public Task Write(string key, byte[] content)
+        if (key == null || content == null)
         {
-            if (key == null || content == null)
-            {
-                throw new ArgumentNullException();
-            }
-
-            if (key == "")
-            {
-                throw new ArgumentOutOfRangeException();
-            }
-
-            Storage[key] = (byte[]) content.Clone();
-            return Task.CompletedTask;
+            throw new ArgumentNullException();
         }
 
-        public Task<byte[]> Read(string key)
+        if (key == "")
         {
-            if (key == null)
-            {
-                throw new ArgumentNullException();
-            }
-
-            if (key == "")
-            {
-                throw new ArgumentOutOfRangeException();
-            }
-
-            return Task.FromResult((byte[]) Storage[key].Clone());
+            throw new ArgumentOutOfRangeException();
         }
 
-        public Task<string> GetPresignedUploadUrl(string key)
+        Storage[key] = (byte[]) content.Clone();
+        return Task.CompletedTask;
+    }
+
+    public Task<byte[]> Read(string key)
+    {
+        if (key == null)
         {
-            throw new NotImplementedException();
+            throw new ArgumentNullException();
         }
 
-        public Task<string> GetPresignedDownloadUrl(string key)
+        if (key == "")
         {
-            throw new NotImplementedException();
+            throw new ArgumentOutOfRangeException();
         }
 
-        public Task<ulong> GetTotalSize()
-        {
-            throw new NotImplementedException();
-        }
+        return Task.FromResult((byte[]) Storage[key].Clone());
+    }
+
+    public Task<string> GetPresignedUploadUrl(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GetPresignedDownloadUrl(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ulong> GetTotalSize()
+    {
+        throw new NotImplementedException();
     }
 }
