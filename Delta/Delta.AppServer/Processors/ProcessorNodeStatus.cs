e7d8557b21d41002fdc211948a6483431466903b
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 10:36:59 +0900

    코드 품질 향상

--- Delta/Delta.AppServer/Processors/ProcessorNodeStatus.cs
+++ Delta/Delta.AppServer/Processors/ProcessorNodeStatus.cs
@@ -1,34 +1,33 @@
 using System;
 using NodaTime;
 
-namespace Delta.AppServer.Processors
+namespace Delta.AppServer.Processors;
+
+public class ProcessorNodeStatus
 {
-    public class ProcessorNodeStatus
+    public ProcessorNodeStatus(Instant timestamp, string status, ProcessorNode processorNode)
     {
-        public ProcessorNodeStatus(Instant timestamp, string status, ProcessorNode processorNode)
-        {
-            Timestamp = timestamp;
-            Status = status;
-            _processorNode = processorNode;
-        }
+        Timestamp = timestamp;
+        Status = status;
+        _processorNode = processorNode;
+    }
 
-        protected ProcessorNodeStatus(Instant timestamp, string status)
-        {
-            Timestamp = timestamp;
-            Status = status;
-        }
+    protected ProcessorNodeStatus(Instant timestamp, string status)
+    {
+        Timestamp = timestamp;
+        Status = status;
+    }
 
-        public long Id { get; set; }
-        public long ProcessorNodeId { get; set; }
-        public Instant Timestamp { get; set; }
-        public string Status { get; set; }
+    public long Id { get; set; }
+    public long ProcessorNodeId { get; set; }
+    public Instant Timestamp { get; set; }
+    public string Status { get; set; }
 
-        private ProcessorNode? _processorNode;
+    private ProcessorNode? _processorNode;
 
-        public virtual ProcessorNode ProcessorNode
-        {
-            get => _processorNode ?? throw new InvalidOperationException(nameof(ProcessorNode));
-            set => _processorNode = value;
-        }
+    public virtual ProcessorNode ProcessorNode
+    {
+        get => _processorNode ?? throw new InvalidOperationException(nameof(ProcessorNode));
+        set => _processorNode = value;
     }
 }
