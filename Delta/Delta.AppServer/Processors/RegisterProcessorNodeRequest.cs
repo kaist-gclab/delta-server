56a5ace9e74b1be5121360a6fb31d91e706c050d
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 10:47:07 +0900

    코드 품질 향상

--- Delta/Delta.AppServer/Processors/RegisterProcessorNodeRequest.cs
+++ Delta/Delta.AppServer/Processors/RegisterProcessorNodeRequest.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 
-namespace Delta.AppServer.Processors
-{
-    public record RegisterProcessorNodeRequest(
-        string Key, IEnumerable<CreateProcessorNodeCapabilityRequest> Capabilities);
-}
+namespace Delta.AppServer.Processors;
+
+public record RegisterProcessorNodeRequest(
+    string Key, IEnumerable<CreateProcessorNodeCapabilityRequest> Capabilities);
