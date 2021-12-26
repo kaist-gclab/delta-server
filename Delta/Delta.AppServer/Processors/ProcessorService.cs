885b26196857df22986a1275174cd44673b674a5
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 11:03:45 +0900

    ProcessorService 코드 품질 향상

--- Delta/Delta.AppServer/Processors/ProcessorService.cs
+++ Delta/Delta.AppServer/Processors/ProcessorService.cs
@@ -3,48 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using NodaTime;
 
-namespace Delta.AppServer.Processors
+namespace Delta.AppServer.Processors;
+
+public class ProcessorService
 {
-    public class ProcessorService
+    private readonly DeltaContext _context;
+    private readonly IClock _clock;
+
+    public ProcessorService(DeltaContext context, IClock clock)
     {
-        private readonly DeltaContext _context;
-        private readonly IClock _clock;
-
-        public ProcessorService(DeltaContext context, IClock clock)
-        {
-            _context = context;
-            _clock = clock;
-        }
-
-        public ProcessorNode? GetNode(long id) => _context.ProcessorNodes.Find(id);
-
-        public ProcessorNode RegisterProcessorNode(RegisterProcessorNodeRequest request)
-        {
-            using var trx = _context.Database.BeginTransaction();
-            var node = (from n in _context.ProcessorNodes
-                           where n.Key == request.Key
-                           select n).FirstOrDefault() ??
-                       _context.Add(new ProcessorNode { Key = request.Key }).Entity;
-
-            node.UpdateCapabilities(request.Capabilities.Select(cap =>
-            {
-                var (jobTypeId, assetTypeId, mediaType) = cap;
-                var jobType = _context.JobTypes.Find(jobTypeId);
-                var assetType = _context.AssetTypes.Find(assetTypeId);
-                if (jobType == null)
-                {
-                    throw new ArgumentException();
-                }
-
-                return new CreateProcessorNodeCapability(jobType, assetType, mediaType);
-            }));
-            node.AddNodeStatus(_clock.GetCurrentInstant(),
-                PredefinedProcessorNodeStatuses.Available);
-            _context.SaveChanges();
-            trx.Commit();
-            return node;
-        }
-
-        public IEnumerable<ProcessorNode> GetProcessorNodes() => _context.ProcessorNodes.ToList();
+        _context = context;
+        _clock = clock;
     }
+
+    public ProcessorNode? GetNode(long id) => _context.ProcessorNodes.Find(id);
+
+    public ProcessorNode RegisterProcessorNode(RegisterProcessorNodeRequest request)
+    {
+        using var trx = _context.Database.BeginTransaction();
+        var node = (from n in _context.ProcessorNodes
+                       where n.Key == request.Key
+                       select n).FirstOrDefault() ??
+                   _context.Add(new ProcessorNode { Key = request.Key }).Entity;
+
+        node.UpdateCapabilities(request.Capabilities.Select(cap =>
+        {
+            var (jobTypeId, assetTypeId, mediaType) = cap;
+            var jobType = _context.JobTypes.Find(jobTypeId);
+            var assetType = _context.AssetTypes.Find(assetTypeId);
+            if (jobType == null)
+            {
+                throw new ArgumentException();
+            }
+
+            return new CreateProcessorNodeCapability(jobType, assetType, mediaType);
+        }));
+        node.AddNodeStatus(_clock.GetCurrentInstant(),
+            PredefinedProcessorNodeStatuses.Available);
+        _context.SaveChanges();
+        trx.Commit();
+        return node;
+    }
+
+    public IEnumerable<ProcessorNode> GetProcessorNodes() => _context.ProcessorNodes.ToList();
 }
