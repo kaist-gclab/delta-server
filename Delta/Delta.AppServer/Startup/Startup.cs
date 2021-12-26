011e2fd616c1857741c8653659256a0939beee1b
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 11:41:43 +0900

    불필요한 using 제거

--- Delta/Delta.AppServer/Startup/Startup.cs
+++ Delta/Delta.AppServer/Startup/Startup.cs
@@ -14,7 +14,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
--- Delta/Delta.AppServer/Users/UserService.cs
+++ Delta/Delta.AppServer/Users/UserService.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 using Delta.AppServer.Core.Security;
 
