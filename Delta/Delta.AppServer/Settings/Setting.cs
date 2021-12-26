1f62f1a44c81f8e805ea4928d37fa18852ec062a
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 10:39:31 +0900

    Setting 코드 품질 향상

--- Delta/Delta.AppServer/Settings/Setting.cs
+++ Delta/Delta.AppServer/Settings/Setting.cs
@@ -1,16 +1,15 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace Delta.AppServer.Settings
-{
-    public class Setting
-    {
-        public Setting(string key, string value)
-        {
-            Key = key;
-            Value = value;
-        }
+namespace Delta.AppServer.Settings;
 
-        [Key] public string Key { get; set; }
-        public string Value { get; set; }
+public class Setting
+{
+    public Setting(string key, string value)
+    {
+        Key = key;
+        Value = value;
     }
+
+    [Key] public string Key { get; set; }
+    public string Value { get; set; }
 }
