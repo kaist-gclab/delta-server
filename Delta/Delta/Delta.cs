d2437690c4b0c16c7c5c3852e2da66fc9850f7ba
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 10:06:37 +0900

    코드 품질 개선

--- Delta/Delta/Delta.cs
+++ Delta/Delta/Delta.cs
@@ -2,8 +2,8 @@
 {
     public class Delta
     {
-        public static string ServiceName { get; } = "Delta";
-        public static string ApiVersion { get; } = "3";
+        public static string ServiceName => "Delta";
+        public static string ApiVersion => "3";
         public const string ApiRoot = "/api/1/";
     }
 }
