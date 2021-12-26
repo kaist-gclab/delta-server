c1d33a3e8258e31ab0041a56e30b634585a13c20
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 10:34:06 +0900

    코드 품질 향상

--- Delta/Delta.AppServer/ObjectStorage/ObjectStorageController.cs
+++ Delta/Delta.AppServer/ObjectStorage/ObjectStorageController.cs
@@ -2,25 +2,24 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Delta.AppServer.ObjectStorage
+namespace Delta.AppServer.ObjectStorage;
+
+[ApiController]
+[Route(Delta.ApiRoot + "object-storage")]
+public class ObjectStorageController : ControllerBase
 {
-    [ApiController]
-    [Route(Delta.ApiRoot + "object-storage")]
-    public class ObjectStorageController : ControllerBase
+    private readonly IObjectStorageService _objectStorageService;
+
+    public ObjectStorageController(IObjectStorageService objectStorageService)
     {
-        private readonly IObjectStorageService _objectStorageService;
+        _objectStorageService = objectStorageService;
+    }
 
-        public ObjectStorageController(IObjectStorageService objectStorageService)
-        {
-            _objectStorageService = objectStorageService;
-        }
-
-        [HttpGet("upload-url")]
-        public async Task<UploadTicket> GetUploadUrl()
-        {
-            var storeKey = Guid.NewGuid().ToString();
-            var url = await _objectStorageService.GetPresignedUploadUrl(storeKey);
-            return new UploadTicket(url, storeKey);
-        }
+    [HttpGet("upload-url")]
+    public async Task<UploadTicket> GetUploadUrl()
+    {
+        var storeKey = Guid.NewGuid().ToString();
+        var url = await _objectStorageService.GetPresignedUploadUrl(storeKey);
+        return new UploadTicket(url, storeKey);
     }
 }
