27af1c9d87f7d97da622ad692339b369bbdbcc9a
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 10:49:39 +0900

    ProcessorNodeCapability 생성자 추가

--- Delta/Delta.AppServer/Processors/ProcessorNodeCapability.cs
+++ Delta/Delta.AppServer/Processors/ProcessorNodeCapability.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel.DataAnnotations;
+using System;
 using Delta.AppServer.Assets;
 using Delta.AppServer.Jobs;
 
@@ -6,14 +6,48 @@ namespace Delta.AppServer.Processors
 {
     public class ProcessorNodeCapability
     {
+        public ProcessorNodeCapability(
+            string mediaType, ProcessorNode processorNode, JobType jobType, AssetType assetType)
+        {
+            MediaType = mediaType;
+            _processorNode = processorNode;
+            _jobType = jobType;
+            _assetType = assetType;
+        }
+
+        protected ProcessorNodeCapability(string mediaType)
+        {
+            MediaType = mediaType;
+        }
+
         public long Id { get; set; }
         public long ProcessorNodeId { get; set; }
         public long JobTypeId { get; set; }
         public long? AssetTypeId { get; set; }
-        [Required] public string MediaType { get; set; }
+        public string MediaType { get; set; }
 
-        [Required] public virtual ProcessorNode ProcessorNode { get; set; }
-        [Required] public virtual JobType JobType { get; set; }
-        public virtual AssetType AssetType { get; set; }
+        private ProcessorNode _processorNode;
+
+        public virtual ProcessorNode ProcessorNode
+        {
+            get => _processorNode ?? throw new InvalidOperationException(nameof(ProcessorNode));
+            set => _processorNode = value;
+        }
+
+        private JobType _jobType;
+
+        public virtual JobType JobType
+        {
+            get => _jobType ?? throw new InvalidOperationException(nameof(JobType));
+            set => _jobType = value;
+        }
+
+        private AssetType? _assetType;
+
+        public virtual AssetType? AssetType
+        {
+            get => _assetType;
+            set => _assetType = value;
+        }
     }
 }
