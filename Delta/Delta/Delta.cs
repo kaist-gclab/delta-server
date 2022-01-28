a226dbd45ad6097348134395404f58aa500912df
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-01-28 09:51:08 +0900

    4차 연도 개발 준비

--- Delta/Delta/Delta.cs
+++ Delta/Delta/Delta.cs
@@ -3,6 +3,6 @@
 public class Delta
 {
     public static string ServiceName => "Delta";
-    public static string ApiVersion => "3";
+    public static string ApiVersion => "4";
     public const string ApiRoot = "/api/1/";
 }
