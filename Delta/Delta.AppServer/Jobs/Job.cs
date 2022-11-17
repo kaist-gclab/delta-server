bbb35c1a935fedec8931b4d5709162e48ccebe33
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-17 23:37:22 +0900

    Job에서 [Required] 및 Nullable 처리 개선

--- Delta/Delta.AppServer/Jobs/Job.cs
+++ Delta/Delta.AppServer/Jobs/Job.cs
@@ -11,12 +11,12 @@ public class Job
     public long Id { get; set; }
     public long JobTypeId { get; set; }
     public long? InputAssetId { get; set; }
-    [Required] public string JobArguments { get; set; }
+    public string JobArguments { get; set; }
     public Instant CreatedAt { get; set; }
     public long? AssignedProcessorNodeId { get; set; }
 
-    [Required] public virtual JobType JobType { get; set; }
-    public virtual Asset InputAsset { get; set; }
+    public virtual JobType JobType { get; set; }
+    public virtual Asset? InputAsset { get; set; }
     public virtual ICollection<JobExecution> JobExecutions { get; set; } = new HashSet<JobExecution>();
-    public virtual ProcessorNode AssignedProcessorNode { get; set; }
+    public virtual ProcessorNode? AssignedProcessorNode { get; set; }
 }
