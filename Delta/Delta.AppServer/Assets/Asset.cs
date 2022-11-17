a4071b3be5528aab067b7a7b0dd02823caae25d1
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-17 23:34:37 +0900

    Asset에서 [Required] 및 Nullable 처리 개선

--- Delta/Delta.AppServer/Assets/Asset.cs
+++ Delta/Delta.AppServer/Assets/Asset.cs
@@ -12,17 +12,17 @@ public class Asset
     public long Id { get; set; }
     public long AssetTypeId { get; set; }
     public long? EncryptionKeyId { get; set; }
-    [Required] public string MediaType { get; set; }
-    [Required] public string StoreKey { get; set; }
+    public string MediaType { get; set; }
+    public string StoreKey { get; set; }
     public long? ParentJobExecutionId { get; set; }
     public Instant CreatedAt { get; set; }
 
-    [Required] public virtual AssetType AssetType { get; set; }
+    public virtual AssetType AssetType { get; set; }
 
-    public virtual EncryptionKey EncryptionKey { get; set; }
+    public virtual EncryptionKey? EncryptionKey { get; set; }
 
-    public virtual JobExecution ParentJobExecution { get; set; }
-    public virtual ICollection<Job> InputJobs { get; set; }
+    public virtual JobExecution? ParentJobExecution { get; set; }
+    public virtual ICollection<Job> InputJobs { get; set; } = new HashSet<Job>();
     public virtual ICollection<AssetTag> AssetTags { get; set; } = new HashSet<AssetTag>();
 
     public void UpdateAssetTag(string key, string value)
