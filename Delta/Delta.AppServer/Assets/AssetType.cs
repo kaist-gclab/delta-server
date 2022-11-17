879f88d28da0cf0276e3a9e62c8a49df2f71a080
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-17 23:35:52 +0900

    AssetType에서 [Required] 및 Nullable 처리 개선

--- Delta/Delta.AppServer/Assets/AssetType.cs
+++ Delta/Delta.AppServer/Assets/AssetType.cs
@@ -7,9 +7,9 @@ namespace Delta.AppServer.Assets;
 public class AssetType
 {
     public long Id { get; set; }
-    [Required] public string Key { get; set; }
-    [Required] public string Name { get; set; }
+    public string Key { get; set; }
+    public string Name { get; set; }
 
-    public virtual ICollection<Asset> Assets { get; set; }
-    public virtual ICollection<ProcessorNodeCapability> ProcessorNodeCapabilities { get; set; }
+    public virtual ICollection<Asset> Assets { get; set; } = new HashSet<Asset>();
+    public virtual ICollection<ProcessorNodeCapability> ProcessorNodeCapabilities { get; set; } = new HashSet<ProcessorNodeCapability>();
 }
