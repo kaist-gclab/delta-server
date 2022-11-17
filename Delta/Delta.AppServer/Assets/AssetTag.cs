4b977a0221b03cc69b8af402fc87d6bba3d5f4e4
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-17 23:35:18 +0900

    AssetTag에서 [Required] 및 Nullable 처리 개선

--- Delta/Delta.AppServer/Assets/AssetTag.cs
+++ Delta/Delta.AppServer/Assets/AssetTag.cs
@@ -6,8 +6,8 @@ public class AssetTag
 {
     public long Id { get; set; }
     public long AssetId { get; set; }
-    [Required] public string Key { get; set; }
-    [Required] public string Value { get; set; }
+    public string Key { get; set; }
+    public string Value { get; set; }
 
-    [Required] public virtual Asset Asset { get; set; }
+    public virtual Asset Asset { get; set; }
 }
