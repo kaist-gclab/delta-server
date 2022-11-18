b1a7b588a52ad3fbfa0861d6aa5077d3d2120ff1
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-18 14:08:03 +0900

    Assets의 불필요한 using 제거

--- Delta/Delta.AppServer/Assets/Asset.cs
+++ Delta/Delta.AppServer/Assets/Asset.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using Delta.AppServer.Jobs;
 using Delta.AppServer.Encryption;
--- Delta/Delta.AppServer/Assets/AssetTag.cs
+++ Delta/Delta.AppServer/Assets/AssetTag.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Delta.AppServer.Assets;
 
 public class AssetTag
