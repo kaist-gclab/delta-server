a8cff163ae40fc452bb491ce1c6f903d750ee907
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-18 13:55:14 +0900

    NullableReferenceTypes

---
+++ Delta/Delta.AppServer/Migrations/20221118045419_NullableReferenceTypes.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Delta.AppServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullableReferenceTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "status",
+                table: "job_execution_status",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "status",
+                table: "job_execution_status",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
