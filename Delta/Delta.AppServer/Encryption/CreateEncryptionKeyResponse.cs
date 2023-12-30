46d497e8e29cd26b6fa19bb7a8424c62392884e2
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 04:18:49 +0900

    CreateEncryptionKeyResponse를 record로 변경

--- Delta/Delta.AppServer/Encryption/CreateEncryptionKeyResponse.cs
+++ Delta/Delta.AppServer/Encryption/CreateEncryptionKeyResponse.cs
@@ -1,7 +1,3 @@
 namespace Delta.AppServer.Encryption;
 
-public class CreateEncryptionKeyResponse
-{
-    public EncryptionKey EncryptionKey { get; set; }
-    public string Value { get; set; }
-}
+public record CreateEncryptionKeyResponse(EncryptionKeyView EncryptionKey, string Value);
