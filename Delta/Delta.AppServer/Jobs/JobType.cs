efa05fb9586f85759e41ea806f422097a918675e
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 05:23:59 +0900

    Name을 required로 표시

--- Delta/Delta.AppServer/Jobs/JobType.cs
+++ Delta/Delta.AppServer/Jobs/JobType.cs
@@ -6,8 +6,8 @@ namespace Delta.AppServer.Jobs;
 public class JobType
 {
     public long Id { get; set; }
-    public string Name { get; set; }
     public required string Key { get; set; }
+    public required string Name { get; set; }
 
     public virtual ICollection<Job> Jobs { get; set; } = new HashSet<Job>();
     public virtual ICollection<ProcessorNodeCapability> ProcessorNodeCapabilities { get; set; } = new HashSet<ProcessorNodeCapability>();
