42ccfa9d268423f94f48e34b4d389315a2ccecfd
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 04:16:47 +0900

    ComputeSHA256 유틸리티 추가

--- Delta/Delta.AppServer/Core/Security/SecurityHelper.cs
+++ Delta/Delta.AppServer/Core/Security/SecurityHelper.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Delta.AppServer.Core.Security;
 
 public static class SecurityHelper
 {
+    public static string ComputeSHA256(this string text)
+    {
+        var hash = SHA256.HashData(Encoding.UTF8.GetBytes(text));
+        return hash.ToHexString();
+    }
+
     public static string ToHexString(this byte[] bytes)
     {
         return BitConverter.ToString(bytes).Replace("-", "").ToLower();
