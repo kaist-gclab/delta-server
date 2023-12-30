41e797a755d3a8fd4fc53ad0397ec73cc2eb04de
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 04:57:28 +0900

    생성자 제거

--- Delta/Delta.AppServer/Settings/Setting.cs
+++ Delta/Delta.AppServer/Settings/Setting.cs
@@ -4,12 +4,6 @@ namespace Delta.AppServer.Settings;
 
 public class Setting
 {
-    public Setting(string key, string value)
-    {
-        Key = key;
-        Value = value;
-    }
-
     [Key] public string Key { get; set; }
     public string Value { get; set; }
 }
