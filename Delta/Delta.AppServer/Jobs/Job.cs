bf89118166c59ce099c33cf446297fa943382664
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 04:33:16 +0900

    InputAssetId 속성 제거

--- Delta/Delta.AppServer/Jobs/Job.cs
+++ Delta/Delta.AppServer/Jobs/Job.cs
@@ -8,7 +8,6 @@ namespace Delta.AppServer.Jobs;
 public class Job
 {
     public long Id { get; set; }
-    public long? InputAssetId { get; set; }
     public string JobArguments { get; set; }
     public Instant CreatedAt { get; set; }
     public long? AssignedProcessorNodeId { get; set; }
