58a1b539dc1a7dcc2a5e8836d47d5aecb5f32857
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 05:25:51 +0900

    Key를 required로 표시

--- Delta/Delta.AppServer/Assets/AssetType.cs
+++ Delta/Delta.AppServer/Assets/AssetType.cs
@@ -6,8 +6,8 @@ namespace Delta.AppServer.Assets;
 public class AssetType
 {
     public long Id { get; set; }
-    public string Key { get; set; }
     public string Name { get; set; }
+    public required string Key { get; set; }
 
     public virtual ICollection<Asset> Assets { get; set; } = new HashSet<Asset>();
     public virtual ICollection<ProcessorNodeCapability> ProcessorNodeCapabilities { get; set; } = new HashSet<ProcessorNodeCapability>();
