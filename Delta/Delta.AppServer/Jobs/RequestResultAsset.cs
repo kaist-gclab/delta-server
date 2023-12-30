744415a7f3effe5f09b413acb0aee0d2516725c1
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 05:25:16 +0900

    RequestResultAsset를 record로 변경

--- Delta/Delta.AppServer/Jobs/RequestResultAsset.cs
+++ Delta/Delta.AppServer/Jobs/RequestResultAsset.cs
@@ -2,10 +2,9 @@ using System.Collections.Generic;
 
 namespace Delta.AppServer.Jobs;
 
-public class RequestResultAsset
-{
-    public string AssetFormatKey { get; set; }
-    public string AssetTypeKey { get; set; }
-    public IEnumerable<RequestAssetTag> AssetTags { get; set; } = new HashSet<RequestAssetTag>();
-    public byte[] Content { get; set; }
-}
+public record RequestResultAsset(
+    string AssetFormatKey,
+    string AssetTypeKey,
+    IEnumerable<RequestAssetTag> AssetTags,
+    byte[] Content
+);
