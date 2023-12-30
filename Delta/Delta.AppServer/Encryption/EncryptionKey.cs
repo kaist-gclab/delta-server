94802a04389d42d054e3fd9da68a34d7ca4b9823
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 05:23:16 +0900

    Enabled를 required로 표시

--- Delta/Delta.AppServer/Encryption/EncryptionKey.cs
+++ Delta/Delta.AppServer/Encryption/EncryptionKey.cs
@@ -9,7 +9,7 @@ public class EncryptionKey
     public long Id { get; set; }
     public required string Name { get; set; }
     [JsonIgnore] public string? Value { get; set; }
-    public bool Enabled { get; set; }
+    public required bool Enabled { get; set; }
 
     public virtual ICollection<Asset> Assets { get; set; } = new HashSet<Asset>();
 }
