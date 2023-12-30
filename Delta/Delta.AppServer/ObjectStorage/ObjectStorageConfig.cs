187a3311ffb91c53a2bea4b1001ca053b283823b
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 05:07:56 +0900

    코드 품질 향상

--- Delta/Delta.AppServer/ObjectStorage/ObjectStorageConfig.cs
+++ Delta/Delta.AppServer/ObjectStorage/ObjectStorageConfig.cs
@@ -12,7 +12,6 @@ public class ObjectStorageConfig
         _configuration = configuration;
     }
 
-
     public string Endpoint => _configuration["ObjectStorage:Endpoint"] ??
                               throw new InvalidOperationException();
 
