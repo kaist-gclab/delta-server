db0fcc5f2da1d990c71ed3ce644236825e844f04
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 04:54:46 +0900

    AssetId 속성 제거

--- Delta/Delta.AppServer/Assets/AssetTag.cs
+++ Delta/Delta.AppServer/Assets/AssetTag.cs
@@ -3,7 +3,6 @@ namespace Delta.AppServer.Assets;
 public class AssetTag
 {
     public long Id { get; set; }
-    public long AssetId { get; set; }
     public string Key { get; set; }
     public string Value { get; set; }
 
