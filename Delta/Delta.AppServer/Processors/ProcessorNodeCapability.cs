f1217031d118815a96bc546f64d45d580fae1a4d
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 05:10:08 +0900

    코드 품질 향상

--- Delta/Delta.AppServer/Processors/ProcessorNodeCapability.cs
+++ Delta/Delta.AppServer/Processors/ProcessorNodeCapability.cs
@@ -8,28 +8,7 @@ public class ProcessorNodeCapability
 {
     public long Id { get; set; }
     public string MediaType { get; set; }
-
-    private ProcessorNode _processorNode;
-
-    public virtual ProcessorNode ProcessorNode
-    {
-        get => _processorNode ?? throw new InvalidOperationException(nameof(ProcessorNode));
-        set => _processorNode = value;
-    }
-
-    private JobType _jobType;
-
-    public virtual JobType JobType
-    {
-        get => _jobType ?? throw new InvalidOperationException(nameof(JobType));
-        set => _jobType = value;
-    }
-
-    private AssetType? _assetType;
-
-    public virtual AssetType? AssetType
-    {
-        get => _assetType;
-        set => _assetType = value;
-    }
+    public virtual ProcessorNode ProcessorNode { get; set; }
+    public virtual JobType JobType { get; set; }
+    public virtual AssetType? AssetType { get; set; }
 }
