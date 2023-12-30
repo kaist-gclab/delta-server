98123a933a1e734cb0c4487979b9db166041a186
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 05:11:09 +0900

    AssetType을 required로 표시

--- Delta/Delta.AppServer/Processors/ProcessorNodeCapability.cs
+++ Delta/Delta.AppServer/Processors/ProcessorNodeCapability.cs
@@ -7,8 +7,8 @@ namespace Delta.AppServer.Processors;
 public class ProcessorNodeCapability
 {
     public long Id { get; set; }
-    public virtual AssetType? AssetType { get; set; }
     public required string MediaType { get; set; }
     public virtual required ProcessorNode ProcessorNode { get; set; }
     public virtual required JobType JobType { get; set; }
+    public virtual required AssetType? AssetType { get; set; }
 }
