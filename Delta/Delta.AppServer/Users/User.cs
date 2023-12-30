f83606127a1745cd890823682c47095b950a2759
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 04:58:31 +0900

    Name을 required로 표시

--- Delta/Delta.AppServer/Users/User.cs
+++ Delta/Delta.AppServer/Users/User.cs
@@ -9,8 +9,8 @@ public class User
     }
 
     public long Id { get; set; }
-    public string Name { get; set; }
     public string Username { get; set; }
+    public required string Name { get; set; }
 
     public string? Password { get; set; }
     public string? Salt { get; set; }
