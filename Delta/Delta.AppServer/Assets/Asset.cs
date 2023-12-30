14231569c6a5b051bd24622938cec06dcbb4c9f3
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 04:03:02 +0900

    ParentJobExecution을 required로 표시

--- Delta/Delta.AppServer/Assets/Asset.cs
+++ Delta/Delta.AppServer/Assets/Asset.cs
@@ -18,7 +18,7 @@ public class Asset
 
     public virtual EncryptionKey? EncryptionKey { get; set; }
 
-    public virtual JobExecution? ParentJobExecution { get; set; }
+    public virtual required JobExecution? ParentJobExecution { get; set; }
     public virtual ICollection<Job> InputJobs { get; set; } = new HashSet<Job>();
     public virtual ICollection<AssetTag> AssetTags { get; set; } = new HashSet<AssetTag>();
 
