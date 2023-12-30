dba7285e2e3fc531c9091218c9cb545c05d77d43
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 04:55:06 +0900

    Key를 required로 표시

--- Delta/Delta.AppServer/Assets/AssetTag.cs
+++ Delta/Delta.AppServer/Assets/AssetTag.cs
@@ -3,8 +3,8 @@ namespace Delta.AppServer.Assets;
 public class AssetTag
 {
     public long Id { get; set; }
-    public string Key { get; set; }
     public string Value { get; set; }
+    public required string Key { get; set; }
 
     public virtual Asset Asset { get; set; }
 }
