c5686fbf9e4a2fbbb1c1fe2e160dd1629d655108
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 04:54:00 +0900

    JobId 속성 제거

--- Delta/Delta.AppServer/Jobs/JobExecution.cs
+++ Delta/Delta.AppServer/Jobs/JobExecution.cs
@@ -9,7 +9,6 @@ namespace Delta.AppServer.Jobs;
 public class JobExecution
 {
     public long Id { get; set; }
-    public long JobId { get; set; }
     public long ProcessorNodeId { get; set; }
 
     public virtual Job Job { get; set; }
