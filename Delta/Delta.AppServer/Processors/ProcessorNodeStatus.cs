55869a79a11f2f92cae90d79de50260ba557777c
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 05:13:15 +0900

    ProcessorNode를 required로 표시

--- Delta/Delta.AppServer/Processors/ProcessorNodeStatus.cs
+++ Delta/Delta.AppServer/Processors/ProcessorNodeStatus.cs
@@ -6,14 +6,7 @@ namespace Delta.AppServer.Processors;
 public class ProcessorNodeStatus
 {
     public long Id { get; set; }
-
-    private ProcessorNode? _processorNode;
-
-    public virtual ProcessorNode ProcessorNode
-    {
-        get => _processorNode ?? throw new InvalidOperationException(nameof(ProcessorNode));
-        set => _processorNode = value;
-    }
     public required Instant Timestamp { get; set; }
     public required string Status { get; set; }
+    public virtual required ProcessorNode ProcessorNode { get; set; }
 }
