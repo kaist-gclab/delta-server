2947cf582e611d351579b50e263e78dd2720b4b4
Hyeonguk Ryu <ryu@hyeonguk.com>
2024-11-17 20:20:07 +0900

    빌드 복구

--- Delta/Delta.AppServer/ObjectStorage/S3CompatibleObjectStorageService.cs
+++ Delta/Delta.AppServer/ObjectStorage/S3CompatibleObjectStorageService.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Minio;
+using Minio.ApiEndpoints;
 using Minio.DataModel.Args;
 
 namespace Delta.AppServer.ObjectStorage;
