7eafd8e91a79b53aeebe7fe7a42029bfd74b3007
Hyeonguk Ryu <ryu@hyeonguk.com>
2024-11-19 14:18:42 +0900

    AuthController에 primary constructor 적용

--- Delta/Delta.AppServer/Core/Security/AuthController.cs
+++ Delta/Delta.AppServer/Core/Security/AuthController.cs
@@ -7,28 +7,19 @@ namespace Delta.AppServer.Core.Security;
 
 [Route("/auth/1/")]
 [ApiController]
-public class AuthController : ControllerBase
+public class AuthController(TokenService tokenService, UserService userService) : ControllerBase
 {
-    private readonly UserService _userService;
-    private readonly TokenService _tokenService;
-
-    public AuthController(TokenService tokenService, UserService userService)
-    {
-        _tokenService = tokenService;
-        _userService = userService;
-    }
-
     [HttpPost]
     [Route("login")]
     [AllowAnonymous]
     public async Task<ActionResult<LoginResponse>> Login([FromBody] LoginRequest loginRequest)
     {
-        var user = await _userService.Login(loginRequest.Username, loginRequest.Password);
+        var user = await userService.Login(loginRequest.Username, loginRequest.Password);
         if (user == null)
         {
             return Unauthorized();
         }
 
-        return Ok(new LoginResponse(_tokenService.BuildToken(user)));
+        return Ok(new LoginResponse(tokenService.BuildToken(user)));
     }
 }
