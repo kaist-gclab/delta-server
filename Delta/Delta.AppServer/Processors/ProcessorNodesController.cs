f9a5edbea2e51f401eec493457cf63450089f1b8
Hyeonguk Ryu <ryu@hyeonguk.com>
2024-11-19 14:19:34 +0900

    ProcessorNodesController에 primary constructor 적용

--- Delta/Delta.AppServer/Processors/ProcessorNodesController.cs
+++ Delta/Delta.AppServer/Processors/ProcessorNodesController.cs
@@ -6,26 +6,19 @@ namespace Delta.AppServer.Processors;
 
 [ApiController]
 [Route(Delta.ApiRoot + "processor-nodes")]
-public class ProcessorNodesController : ControllerBase
+public class ProcessorNodesController(ProcessorService processorService) : ControllerBase
 {
-    private readonly ProcessorService _processorService;
-
-    public ProcessorNodesController(ProcessorService processorService)
-    {
-        _processorService = processorService;
-    }
-
     [HttpGet]
     public async Task<IEnumerable<ProcessorNode>> GetNodes()
     {
-        return await _processorService.GetProcessorNodes();
+        return await processorService.GetProcessorNodes();
     }
 
     [HttpPost("register")]
     public async Task<ActionResult<ProcessorNode>> RegisterProcessorNode(
         RegisterProcessorNodeRequest registerProcessorNodeRequest)
     {
-        var node = await _processorService.RegisterProcessorNode(registerProcessorNodeRequest);
+        var node = await processorService.RegisterProcessorNode(registerProcessorNodeRequest);
         if (node == null)
         {
             return BadRequest();
