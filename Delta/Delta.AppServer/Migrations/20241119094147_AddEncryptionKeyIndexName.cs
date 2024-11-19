2908af598c6f0e0c444ed28e8df3c404b13f83f2
Hyeonguk Ryu <ryu@hyeonguk.com>
2024-11-19 18:43:08 +0900

    AddEncryptionKeyIndexName

---
+++ Delta/Delta.AppServer/Migrations/20241119094147_AddEncryptionKeyIndexName.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Delta.AppServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEncryptionKeyIndexName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_asset_encryption_key_encryption_key_id",
+                table: "asset");
+
+            migrationBuilder.DropIndex(
+                name: "IX_asset_encryption_key_id",
+                table: "asset");
+
+            migrationBuilder.DropColumn(
+                name: "encryption_key_id",
+                table: "asset");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_encryption_key_name",
+                table: "encryption_key",
+                column: "name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_encryption_key_name",
+                table: "encryption_key");
+
+            migrationBuilder.AddColumn<long>(
+                name: "encryption_key_id",
+                table: "asset",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_asset_encryption_key_id",
+                table: "asset",
+                column: "encryption_key_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_asset_encryption_key_encryption_key_id",
+                table: "asset",
+                column: "encryption_key_id",
+                principalTable: "encryption_key",
+                principalColumn: "id");
+        }
+    }
+}
