14417386c26a186fb036952697935ded72591d11
Hyeonguk Ryu <ryu@hyeonguk.com>
2024-11-19 14:19:29 +0900

    ObjectStorageConfig에 primary constructor 적용

--- Delta/Delta.AppServer/ObjectStorage/ObjectStorageConfig.cs
+++ Delta/Delta.AppServer/ObjectStorage/ObjectStorageConfig.cs
@@ -3,27 +3,20 @@ using Microsoft.Extensions.Configuration;
 
 namespace Delta.AppServer.ObjectStorage;
 
-public class ObjectStorageConfig
+public class ObjectStorageConfig(IConfiguration configuration)
 {
-    private readonly IConfiguration _configuration;
-
-    public ObjectStorageConfig(IConfiguration configuration)
-    {
-        _configuration = configuration;
-    }
-
-    public string Endpoint => _configuration["ObjectStorage:Endpoint"] ??
+    public string Endpoint => configuration["ObjectStorage:Endpoint"] ??
                               throw new InvalidOperationException();
 
-    public string AccessKey => _configuration["ObjectStorage:AccessKey"] ??
+    public string AccessKey => configuration["ObjectStorage:AccessKey"] ??
                                throw new InvalidOperationException();
 
-    public string SecretKey => _configuration["ObjectStorage:SecretKey"] ??
+    public string SecretKey => configuration["ObjectStorage:SecretKey"] ??
                                throw new InvalidOperationException();
 
-    public string Bucket => _configuration["ObjectStorage:Bucket"] ??
+    public string Bucket => configuration["ObjectStorage:Bucket"] ??
                             throw new InvalidOperationException();
 
-    public bool Https => bool.Parse(_configuration["ObjectStorage:Https"] ??
+    public bool Https => bool.Parse(configuration["ObjectStorage:Https"] ??
                                     throw new InvalidOperationException());
 }
